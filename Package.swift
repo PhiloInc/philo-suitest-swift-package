634d2615ab2f8cadf65de033b4d6642100056b12
David Thompson <dave@philo.com>
2021-08-06 00:22:36 -0400

    try adding a second binaryTarget

--- Package.swift
+++ Package.swift
@@ -25,5 +25,10 @@ let package = Package(
             url: "https://file.suite.st/tvos/xcframework/1.6.2/Suitest.xcframework.zip",
             // compute using "swift package compute-checksum local-zipfile-path"
             checksum: "18b85826447bb6b60b82e190e4f6c7da7f9f0f2ffe49de6ed719045ec18c0574"),
+        .binaryTarget(
+            name: "Suitest_iOS",
+            url: "https://file.suite.st/tvos/xcframework/1.6.2/Suitest.xcframework.zip",
+            // compute using "swift package compute-checksum local-zipfile-path"
+            checksum: "18b85826447bb6b60b82e190e4f6c7da7f9f0f2ffe49de6ed719045ec18c0574"),
     ]
 )
